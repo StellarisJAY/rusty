b6cc6858e939f22ddf9fae0df088fdfa83a0af38
xxjay <760409558@qq.com>
2023-01-18 14:37:14 +0800

    feat: Add info! warn! debug! error!

--- os/src/console.rs
+++ os/src/console.rs
@@ -15,6 +15,9 @@ impl Write for StdOut {
 pub fn print(args: fmt::Arguments) {
     StdOut.write_fmt(args).unwrap();
 }
+pub fn print_str(s: &str) {
+    StdOut.write_str(s).unwrap();
+}
 
 #[macro_export]
 macro_rules! print {
@@ -27,5 +30,41 @@ macro_rules! print {
 macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! error {
+    ($fmt: literal $(, $($arg: tt)+)?)=>{
+        $crate::console::print_str("\x1b[31m[ERROR] ");
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+        $crate::console::print_str("\x1b[0m\n");
+    }
+}
+
+#[macro_export]
+macro_rules! info {
+    ($fmt: literal $(, $($arg: tt)+)?)=>{
+        $crate::console::print_str("[INFO] ");
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+        $crate::console::print_str("\n");
+    }
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($fmt: literal $(, $($arg: tt)+)?)=>{
+        $crate::console::print_str("\x1b[93m[WARN] ");
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+        $crate::console::print_str("\x1b[0m\n");
+    }
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($fmt: literal $(, $($arg: tt)+)?)=>{
+        $crate::console::print_str("\x1b[32m[DEBUG] ");
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+        $crate::console::print_str("\x1b[0m\n");
     }
 }
