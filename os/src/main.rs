39e149468233cf2120e7f511fb4112f51794a812
xxjay <760409558@qq.com>
2023-01-18 11:39:45 +0800

    feat: 启动主函数和清空bss段

--- os/src/main.rs
+++ os/src/main.rs
@@ -7,3 +7,28 @@ use core::arch::global_asm;
 
 // entry point: entry.asm
 global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    loop{}
+}
+
+// 清空bss段
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    } {
+        let start = sbss as usize;
+        let end = ebss as usize;
+        let mut current = start;
+        while current <= end {
+            unsafe {
+                // 将current作为地址，转换成指针后修改数据的值为0
+                (current as *mut u8).write_volatile(0);
+            }
+            current += 1;
+        }
+    }
+}
