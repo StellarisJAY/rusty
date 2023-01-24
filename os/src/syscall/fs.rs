bb7c00d19aa108292bae546ad51c4237e693e9fc
xxjay <760409558@qq.com>
2023-01-24 14:44:19 +0800

    feat: write syscall增加错误检查

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -4,9 +4,19 @@ pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
-            let str = core::str::from_utf8(slice).unwrap();
-            print!("{}", str);
-            len as isize
+            if slice.len() == 0 {
+                return 0;
+            }
+            match core::str::from_utf8(slice) {
+                Ok(str) => {
+                    print!("{}", str);
+                    return len as isize;
+                },
+                Err(error) => {
+                    error!("interpret slice as utf-8 string error: {}", error);
+                    0
+                },
+            }
         },
         _ => {
             panic!("Unsupported fd in sys_write!");
