7c08f452bde5b542de728409bd332972eb6303d4
xxjay <760409558@qq.com>
2023-01-24 14:43:12 +0800

    feat: user_lib增加获取时间syscall

--- os/src/syscall/task.rs
+++ os/src/syscall/task.rs
@@ -1,5 +1,5 @@
 use crate::task::{suspend_current_task, exit_current_task, run_next_task};
-
+use crate::timer::get_time_ms;
 pub fn sys_exit(xstate: i32) -> ! {
     kernel_info!("Application exited with code {}", xstate);
     exit_current_task();
@@ -10,4 +10,8 @@ pub fn sys_yield() -> isize{
     debug!("application yield");
     suspend_current_task();
     run_next_task();
+}
+
+pub fn sys_get_time() -> isize {
+    get_time_ms() as isize
 }
