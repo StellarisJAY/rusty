e9fb983ea4e1090780155b9bf74460b124b48492
xxjay <760409558@qq.com>
2023-01-26 11:45:15 +0800

    feat: 虚拟页号到物理页的映射

--- os/src/mem/address.rs
+++ os/src/mem/address.rs
@@ -1,4 +1,5 @@
 use crate::config::{PAGE_OFFSET_MASK, PAGE_SIZE_BITS, PAGE_SIZE};
+use super::page_table::PageTableEntry;
 
 const RISCV_PPN_WIDTH: usize = 44;
 // RISCV物理地址长度，56位。
@@ -32,16 +33,35 @@ impl VirtAddr {
     pub fn page_offset(&self) -> usize {
         self.0 & PAGE_OFFSET_MASK
     }
-    pub fn floor(&self) -> VirtPageNumber { VirtPageNumber(self.0 / PAGE_SIZE) }
-    pub fn ceil(&self) -> VirtPageNumber { VirtPageNumber((self.0 + PAGE_SIZE - 1) / PAGE_SIZE) }
 }
 
 
 impl PhysPageNumber {
     // 将一个物理页作为mutable切片返回
-    pub fn as_bytes_array(&self) -> &mut [u8] {
+    pub fn as_bytes_array(&self) -> &'static mut [u8] {
         let start_ptr = ((self.0 * PAGE_SIZE) as usize)  as *mut u8;
         unsafe {core::slice::from_raw_parts_mut(start_ptr, PAGE_SIZE)}
+    }
+    // 将一个物理页作为多级页表的页表项数组返回
+    // 一个物理页（4KiB）可以容纳 512个页表项（8 字节）
+    pub fn as_pte_array(&self) -> &'static mut [PageTableEntry] {
+        let ptr = ((self.0 * PAGE_SIZE) as usize)  as *mut PageTableEntry;
+        unsafe{core::slice::from_raw_parts_mut(ptr, PAGE_SIZE / 8)}
+    }
+}
+
+impl VirtPageNumber {
+    // 获取三级页表的三个虚拟页号
+    // 每个虚拟页号为9位，可以映射512个物理页
+    pub fn level_indexes(&self) -> [usize; 3] {
+        let mut vpn = self.0;
+        let mut idxs: [usize; 3] = [0; 3];
+        // 低位是更高级的页表，所以需要rev
+        for i in (0..3).rev() {
+            idxs[i] = vpn & 0x1ff;
+            vpn = vpn >> 9;
+        }
+        return idxs;
     }
 }
 
--- os/src/mem/page_table.rs
+++ os/src/mem/page_table.rs
@@ -1,5 +1,9 @@
+#[allow(unused)]
 use bitflags::*;
 use super::address::*;
+use super::frame_allocator::{FrameTracker, alloc_frame, dealloc_frame};
+use alloc::vec::Vec;
+use alloc::vec;
 
 bitflags! {
     pub struct PTEFlags: u8 {
@@ -18,16 +22,23 @@ bitflags! {
 const ENTRY_PPN_MASK: usize = 0x3ffffffffffc00;
 
 // PageEntry，一个页表项大小为8字节
-// 页表基地址位PT_BASE_ADDR，每个虚拟页号 i 对应的页表项地址：PT_BASE_ADDR + 8*i
-// 多级页表的页表项中的PPN表示下一级页表的虚拟页号
-// 最后一级页表的PPN为物理页号，物理页地址 = PPN * 4KiB + BASE_ADDR
+// 非叶子节点的页表项的ppn表示下一级页表的物理页号
+// 叶子节点页表项的ppn表示虚拟页映射的物理页的页号
 #[derive(Clone, Copy)]
 #[repr(C)]
-struct PageEntry {
+pub struct PageTableEntry {
     pub bits: usize
 }
 
-impl PageEntry {
+// PageTable，三级页表根节点
+// 每级页表用一个物理页保存，所以根节点需要一个root_ppn
+// 一个物理页4KiB，一个页表项8B，所以一级页表可以容纳512个页表项
+pub struct PageTable {
+    root_ppn: PhysPageNumber,
+    frames: Vec<FrameTracker>,
+}
+
+impl PageTableEntry {
     pub fn new(ppn: PhysPageNumber, flags: PTEFlags) -> Self {
         Self { bits:  ppn.0 << 10 | flags.bits as usize}
     }
@@ -43,5 +54,70 @@ impl PageEntry {
 
     pub fn is_valid(&self) -> bool {
         (self.flags() & PTEFlags::V) != PTEFlags::empty()
+    }
+}
+
+impl PageTable {
+    pub fn new() -> Self {
+        let frame = alloc_frame().unwrap();
+        Self { root_ppn: frame.ppn, frames: vec![frame] }
+    }
+    pub fn map(&mut self, vpn: VirtPageNumber, ppn: PhysPageNumber, flags: PTEFlags) {
+        let pte = self.find_or_create_pte(vpn).unwrap();
+        assert!(!pte.is_valid(), "vpn: {} already mapped before mapping", vpn.0);
+        *pte = PageTableEntry::new(ppn, PTEFlags::V);
+    }
+    pub fn unmap(&mut self, vpn: VirtPageNumber) {
+        let pte = self.find_pte(vpn).unwrap();
+        assert!(pte.is_valid(), "unmapped invalid vpn: {}", vpn.0);
+        *pte = PageTableEntry::empty();
+    }
+
+    fn find_or_create_pte(&mut self, vpn: VirtPageNumber) -> Option<&mut PageTableEntry> {
+        let mut ppn = self.root_ppn;
+        let vpn_idxs = vpn.level_indexes();
+        for i in 0..3 {
+            // 获取当前一级页表的的页表项数组，然后获取vpn对应的页表项
+            let pte = &mut ppn.as_pte_array()[vpn_idxs[i]];
+            if i == 2 {
+                return Some(pte);
+            }
+            // 页表项无效，创建新的页表项，并绑定到物理页
+            if !pte.is_valid() {
+                let frame = alloc_frame().unwrap();
+                *pte = PageTableEntry::new(frame.ppn, PTEFlags::V);
+                self.frames.push(FrameTracker{ppn: frame.ppn});
+            }
+            ppn = pte.page_number();
+        }
+        return None;
+    }
+
+    fn find_pte(&self, vpn: VirtPageNumber) -> Option<&mut PageTableEntry> {
+        let mut ppn = self.root_ppn;
+        let vpn_idxs = vpn.level_indexes();
+        for i in 0..3 {
+            let pte = &mut ppn.as_pte_array()[vpn_idxs[i]];
+            if i == 2 {
+                return Some(pte);
+            }
+            if !pte.is_valid() {
+                return None;
+            }
+            ppn = pte.page_number();
+        }
+        return None;
+    }
+
+    /// Temporarily used to get arguments from user space.
+    pub fn from_token(satp: usize) -> Self {
+        Self {
+            root_ppn: PhysPageNumber(satp & ((1usize << 44) - 1)),
+            frames: Vec::new(),
+        }
+    }
+    pub fn translate(&self, vpn: VirtPageNumber) -> Option<PageTableEntry> {
+        self.find_pte(vpn)
+            .map(|pte| {pte.clone()})
     }
 }
