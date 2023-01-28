c318dbc35bed2cb676d305960ca891d3a6ce6b6b
xxjay <760409558@qq.com>
2023-01-28 15:53:18 +0800

    feat: Trap上下文增加satp、sp虚拟地址、trap_handler地址

--- os/src/trap/context.rs
+++ os/src/trap/context.rs
@@ -7,6 +7,12 @@ pub struct TrapContext {
     pub sstatus: Sstatus,
     // trap结束后跳转到的命令地址
     pub sepc: usize,
+    // satp寄存器值，即该拥有该上下文的任务的页表ppn
+    pub kernel_satp: usize,
+    // 内核栈栈顶虚拟地址
+    pub kernel_sp: usize,
+    // trap处理器入口虚拟地址
+    pub trap_handler: usize,
 }
 
 impl TrapContext {
@@ -26,6 +32,23 @@ impl TrapContext {
         // 将ctx的sp设置成此时用户栈的sp
         // 使restore操作时的sscratch是用户栈sp
         ctx.set_sp(sp);
+        return ctx;
+    }
+
+    pub fn task_init_context(app_entry: usize, user_sp: usize, kernel_sp: usize, kernel_satp: usize, trap_handler: usize) -> Self {
+        let mut sstatus = sstatus::read();
+        sstatus.set_spp(SPP::User);
+        let mut ctx = TrapContext{
+            x: [0; 32],
+            sstatus: sstatus,
+            sepc: app_entry, //离开S后跳回app_entry执行app
+            kernel_satp: kernel_satp,
+            kernel_sp: kernel_sp,
+            trap_handler: trap_handler,
+        };
+        // 将ctx的sp设置成此时用户栈的sp
+        // 使restore操作时的sscratch是用户栈sp
+        ctx.set_sp(user_sp);
         return ctx;
     }
 }
