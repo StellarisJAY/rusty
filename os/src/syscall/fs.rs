869c6b5cb1893398ed83a9ab2873429ec481c3d7
xxjay <760409558@qq.com>
2023-01-29 16:14:54 +0800

    feat: sys_write支持地址空间

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,22 +1,15 @@
+use crate::mem::page_table::translated_byte_buffer;
+use crate::task::current_task_satp;
 const FD_STDOUT: usize = 1;
 
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
-            let slice = unsafe { core::slice::from_raw_parts(buf, len) };
-            if slice.len() == 0 {
-                return 0;
+            let buffers = translated_byte_buffer(current_task_satp(), buf, len);
+            for buffer in buffers {
+                print!("{}", core::str::from_utf8(buffer).unwrap());
             }
-            match core::str::from_utf8(slice) {
-                Ok(str) => {
-                    print!("{}", str);
-                    return len as isize;
-                },
-                Err(error) => {
-                    error!("interpret slice as utf-8 string error: {}", error);
-                    0
-                },
-            }
+            len as isize
         },
         _ => {
             panic!("Unsupported fd in sys_write!");
