fb8f647e10449d4ebde3f44189a99e4ac0cefd33
xxjay <760409558@qq.com>
2023-02-05 12:50:02 +0800

    feat: 应用程序堆分配

--- user_lib/src/lib.rs
+++ user_lib/src/lib.rs
@@ -1,16 +1,33 @@
 #![no_std]
 #![feature(linkage)]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
 mod syscall;
 #[macro_use]
 pub mod console;
 pub mod lang_items;
+// 用户堆内存大小：4MiB
+const USER_HEAP_SIZE: usize = 4096 * 1024;
+
+use buddy_system_allocator::LockedHeap;
+
+// 用户应用程序的堆内存分配
+#[global_allocator]
+static HEAP: LockedHeap = LockedHeap::new();
+static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
+
+#[no_mangle]
+#[alloc_error_handler]
+pub fn heap_alloc_error_handler(layout: core::alloc::Layout) -> ! {
+    panic!("alloc heap memory error: {:?}", layout);
+}
 
 // 将该函数link到.text.entry，即kernel内存的开始位置
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start(){
+    init_user_heap();
     // 执行main，并用main返回的exit_code退出
     exit(main());
 }
@@ -21,6 +38,12 @@ pub extern "C" fn _start(){
 #[no_mangle]
 fn main() -> i32 {
     panic!("cannot find main");
+}
+
+fn init_user_heap() {
+    unsafe {
+        HEAP.lock().init(HEAP_SPACE.as_ptr() as usize, USER_HEAP_SIZE);
+    }
 }
 
 use syscall::*;
@@ -38,4 +61,34 @@ pub fn yield_() -> isize {
 
 pub fn get_time() -> isize {
     sys_get_time()
+}
+
+pub fn wait(exit_code: &mut i32) -> isize {
+    loop {
+        match sys_waitpid(-1, exit_code as *mut _) {
+            -2 => {yield_();},
+            pid => return pid,
+        }
+    }
+}
+
+pub fn waitpid(pid: isize, exit_code: &mut i32) -> isize {
+    loop {
+        match sys_waitpid(pid, exit_code as *mut _) {
+            -2 => {yield_();},
+            exit_pid => return exit_pid,
+        }
+    }
+}
+
+pub fn exec(path: &str)->isize {
+    sys_exec(path)
+}
+
+pub fn fork() -> isize {
+    sys_fork()
+}
+
+pub fn read(fd: usize, buffer: &mut [u8]) -> isize {
+    sys_read(fd, buffer)
 }
